116c44e283ad4db18fd4b216ae358346d941768e
julien willette <julien.willette@gmail.com>
2021-07-26 11:38:56 +0200

    resolve algo A3

---
+++ src/challenges/A3/answer.ts
@@ -0,0 +1,34 @@
+/**
+* In this challenge, you should sort messages by their sentAt property (oldest first) and 
+* modify them by adding an "unread" property
+* (boolean meaning that the current user did not read this message) based on the lastActivityDatetime
+* input. 
+*
+* @param lastActivityDatetime String representing an ISO8601 datetime. Represent the last time the user checked his messages
+* @param messages List of messages, unsorted and without unread property
+* @returns Sorted list of messages with the unread information
+*/
+
+// ↓ uncomment bellow lines and add your response!
+
+export default function ({ lastActivityDatetime, messages } : { lastActivityDatetime: string, messages: Message[] }): MessageWithUnread[] {
+    messages.sort(function(a,b){
+        const dateA = new Date(a.sentAt)
+        const dateB = new Date(b.sentAt)
+        return dateA.getTime() - dateB.getTime();
+    })
+    return messages.map(message => {
+        return { ...message, unread: message.sentAt > lastActivityDatetime }
+    })
+}
+
+// used interfaces, do not touch
+export interface Message {
+    author: string;
+    sentAt: string;
+    message: string;
+}
+
+export interface MessageWithUnread extends Message {
+    unread: boolean;
+}
